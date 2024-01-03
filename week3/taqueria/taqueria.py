94c2517af1f26bbf295fcfcf091276f4fee4e5b1
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-01-03 21:05:20 +0100

    taqueria

---
+++ week3/taqueria/taqueria.py
@@ -0,0 +1,39 @@
+
+MENU = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+
+def main():
+
+    total = 0
+
+    while True:
+        try:
+            item = input("Item: ")
+            amount = MENU[item.title()]
+            total += amount
+            print(f"Total: ${total:.2f}")
+        except EOFError:
+            break
+        except KeyError:
+            pass
+
+    print("\n")
+
+
+
+
+main()
+
+
+
+
